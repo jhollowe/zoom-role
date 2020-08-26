6e0bc3c98e218259b526372307b19d79e9eb604b
John Hollowell <johnahollowell@gmail.com>
2020-08-26 03:50:21 +0000

    Add config and init stub

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app
+web: python initialize.py && gunicorn app:app
--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'] or "config.DevelopmentConfig")
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
