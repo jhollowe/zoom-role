8d06d53ee042d6d6ce3bd4e0cbef2eaf8ff40394
John Hollowell <johnahollowell@gmail.com>
2020-08-26 03:56:27 +0000

    Fix config pulling

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'] or "config.DevelopmentConfig")
+app.config.from_object(os.environ.get("APP_SETTINGS", default="config.DevelopmentConfig"))
 
 @app.route('/')
 def hello_world():
